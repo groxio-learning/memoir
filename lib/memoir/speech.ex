08e520594d28905113e1e2de3ac972ff1b210a89
Jeff Weiss <jweiss@enbala.com>
2020-08-18 16:07:49 -0700

    add a couple poems

--- lib/memoir/speech.ex
+++ lib/memoir/speech.ex
@@ -1,7 +1,7 @@
 defmodule Speech do
   def text(:hamlet) do
     """
-    To be or not to be, 
+    To be or not to be,
     that is the question.
     """
   end
@@ -10,6 +10,29 @@ defmodule Speech do
     """
     Darmok and Jalad at Tanagra.
     Shaka, when the walls fell.
+    """
+  end
+
+  def text(:logue) do
+    """
+    Come to the edge.
+    We might fall.
+    Come to the edge.
+    It’s too high!
+    COME TO THE EDGE!
+    And they came,
+    And he pushed,
+    And they flew.
+    """
+  end
+
+  def text(:shikibu) do
+    """
+    Although the wind
+    blows terribly here,
+    the moonlight also leaks
+    between the roof planks
+    of this ruined house.
     """
   end
 end
