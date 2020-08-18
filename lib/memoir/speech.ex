31fabede159a7e4b0a482070063e1c6cc091f6e1
Josh Adams <josh.rubyist@gmail.com>
2020-08-18 18:01:47 -0500

    Add picard speech

--- lib/memoir/speech.ex
+++ lib/memoir/speech.ex
@@ -1,8 +1,15 @@
 defmodule Speech do
-  def hamlet() do
+  def text(:hamlet) do
     """
     To be or not to be, 
     that is the question.
+    """
+  end
+
+  def text(:picard) do
+    """
+    Darmok and Jalad at Tanagra.
+    Shaka, when the walls fell.
     """
   end
 end
